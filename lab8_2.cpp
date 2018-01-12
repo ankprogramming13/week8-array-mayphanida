62d8a16bc14a73f15dc11dc6cd1329effc1a90e5
Phanida Thaijamnong <phanidatha@cvc-cha.ac.th>
2018-01-12 14:11:28 +0700

    ทำข้อ 2 แก้ไข

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -6,8 +6,9 @@ int main () {
 
         char name[] = {"Anukul"};
         int nameLength = 6;
-        cout << name;
+        cout << name << endl;
         char arr[7];
+
 
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
@@ -18,7 +19,17 @@ int main () {
         arr[4] = name[1];
         arr[5] = name[0];
         arr[6] = '\0';
-         cout << arr;
+         cout << arr << endl;
+
+        arr[0] = arr[0] + 1;
+        arr[1] = arr[1] + 1;
+        arr[2] = arr[2] + 1;
+        arr[3] = arr[3] + 1;
+        arr[4] = arr[4] + 1;
+        arr[5] = arr[5] + 1;
+
+
+        cout << arr << endl;
 
 
         return 0;
